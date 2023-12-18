68ccd7cf03bce2fffa73350cad332e149fca2787
110062705 sam <sam990126@gmail.com>
2023-12-18 17:52:39 +0800

    改回原本的檢查

--- server.c
+++ server.c
@@ -16,18 +16,12 @@
 #define group_member_path "server_data/group.txt"
 
 int fileExists(const char *fname) {
-  //   FILE *fp;
-  //   if (fp = fopen(fname, "r")) {
-  //     fclose(fp);
-  //     return 1;
-  //   }
-  //   return 0;
-  struct stat buffer;
-  int exist = stat(fname, &buffer);
-  if (exist == 0)
+  FILE *fp;
+  if (fp = fopen(fname, "r")) {
+    fclose(fp);
     return 1;
-  else
-    return 0;
+  }
+  return 0;
 }
 
 int create_file(char *filename, char *permission, char *group,
